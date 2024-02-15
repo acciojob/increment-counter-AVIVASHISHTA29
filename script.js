587fcea27b1616a2d4ca4c1bf31686786ee0abdf
Avi Vashishta <avivashishta@gmail.com>
2024-02-15 18:35:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let count = 0;
+document.getElementById("incrementBtn")
+	.addEventListener("click",()=>
+	{
+		alert(count);
+		count++;
+		document.getElementById("counter").innerHTML = count;
+		
+	
+})
